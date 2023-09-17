77f7fcd000a62bbfb2ad463db8f1fc6bb136ceb1
nullchamber <131722143+nullchamber@users.noreply.github.com>
2023-09-17 03:54:28 -0500

    Python source for storage.vy

--- contracts/_storage.py
+++ contracts/_storage.py
@@ -1,1 +1,12 @@
-storeData: public()
+storedData: public(int128)
+
+
+@external
+def __init__(_x: int128):
+    self.storedData = _x
+
+
+@external
+def set(_x: int128):
+    self.storedData = _x
+    
